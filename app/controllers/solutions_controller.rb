c7f79a616c2da5e58eaccb1e65ad90391dc47c8f
nyamadori <nyamadorig@gmail.com>
2016-05-30 03:42:47 +0900

    Add controller

---
+++ app/controllers/solutions_controller.rb
@@ -0,0 +1,29 @@
+class SolutionsController < ApplicationController
+  def index
+    # TODO
+  end
+
+  def create
+    # TODO
+  end
+
+  def new
+    # TODO
+  end
+
+  def edit
+    # TODO
+  end
+
+  def show
+    # TODO
+  end
+
+  def update
+    # TODO
+  end
+
+  def destroy
+    # TODO
+  end
+end
