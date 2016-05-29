1be26a452d8946eac7659b0d26a6cca4f17fd704
nyamadori <nyamadorig@gmail.com>
2016-05-30 04:51:20 +0900

    Add validations

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,7 +1,7 @@
 # 問題に対する解答を表す
 class Solution < ActiveRecord::Base
-  # 解答プログラムの判定状態を表す
-  enum :status, { pending: 0, checking: 1, syntax_error: 2 }
+  validates :source, presence: true
 
-  # TODO
+  # 解答プログラムの判定状態を表す
+  enum status: { pending: 0, checking: 1, syntax_error: 2 }
 end
