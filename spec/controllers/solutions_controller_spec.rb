a2786fe55ad43d1b953575d21285bb5f26410404
nyamadori <nyamadorig@gmail.com>
2016-05-30 19:56:21 +0900

    Add spec for solutions#index

--- spec/controllers/solutions_controller_spec.rb
+++ spec/controllers/solutions_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe SolutionsController, type: :controller do
+  describe '#index' do
+    let!(:solutions) do
+      [
+        FactoryGirl.create(:solution, source: '// problem 1'),
+        FactoryGirl.create(:solution, source: '// problem 2')
+      ]
+    end
 
+    it 'populates an array of solutions' do
+      get :index
+      expect(assigns(:solutions)).to match_array(solutions)
+    end
+
+    it 'renders the :index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe '#create'
+  describe '#new'
+  describe '#edit'
+  describe '#show'
+  describe '#update'
+  describe '#destroy'
 end
