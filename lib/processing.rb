1203e25b003c4dfc2c3a74aa620f78ce19ca8386
nyamadori <nyamadorig@gmail.com>
2016-06-06 15:38:27 +0900

    Take a measure for command line injection

--- lib/processing.rb
+++ lib/processing.rb
@@ -1,9 +1,13 @@
+# coding: utf-8
 require 'open3'
+require 'shellwords'
 
 module Processing
   # Processing スケッチをビルドする
   def self.build(sketch_path)
-    _, _, stderr, _ = Open3.popen3("processing-java --sketch=\"#{sketch_path}\" --build")
+    _, _, stderr, _ =
+      Open3.popen3(
+        'processing-java',"--sketch=#{Shellwords.shellescape(sketch_path)}", '--build')
     stderr.read.blank?
   end
 end
